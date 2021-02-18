24ed5480e5f089361ad0426934be5a3a45255886
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-02-18 23:56:40 +1000

    Rough documentation

--- src/Palette.elm
+++ src/Palette.elm
@@ -1,15 +1,30 @@
 module Palette exposing
-    ( Palette
-    , createPalette
-    , getColors
-    , getTags
-    , getTitle
-    , getUrl
+    ( Palette, createPalette
+    , getTitle, getColors, getTags, getAuthorName, getUrl
     )
+
+{-| The Palette type and some useful getters.
+
+
+# Definition
+
+@docs Palette, createPalette
+
+
+# Getters
+
+@docs getTitle, getColors, getTags, getAuthorName, getUrl
+
+-}
 
 import Color exposing (Color)
 
 
+{-| The core type for working with Lospec palettes.
+
+Note that this is an opaque type - see the rest of the functions in this file for ways to work with these
+
+-}
 type Palette
     = Palette
         { title : String
@@ -20,6 +35,11 @@ type Palette
         }
 
 
+{-| Create a palette from some basic properties
+
+If you're looking at this module, you're probably here for the palette list - in which case this won't be much use to you
+
+-}
 createPalette :
     String
     -> List Color
@@ -37,21 +57,48 @@ createPalette title colors tags authorName url =
         }
 
 
+{-| Get the title of a palette
+-}
 getTitle : Palette -> String
 getTitle (Palette palette) =
     palette.title
 
 
+{-| Get the title of a palette
+-}
 getColors : Palette -> List Color
 getColors (Palette palette) =
     palette.colors
 
 
+{-| Get all tags of a palette
+
+While some effort has been taken to keep tags consistent,
+since these are freely input by authors on Lospec there is no guarantee that
+there will be a lot of consistency.
+
+-}
 getTags : Palette -> List String
 getTags (Palette palette) =
     palette.tags
 
 
+{-| Get the name of the creator of a palette
+
+Not all palettes have author names. Some palettes have different formatting for author names.
+If you're attributing a palette creator, I'd recommend linking to the Lospec palette page instead.
+
+-}
+getAuthorName : Palette -> Maybe String
+getAuthorName (Palette palette) =
+    palette.authorName
+
+
+{-| Get the Lospec URL for a palette
+
+If you're specifically using a palette somewhere, show the author some love and link back to it!
+
+-}
 getUrl : Palette -> String
 getUrl (Palette palette) =
     palette.url
--- src/Palettes.elm
+++ src/Palettes.elm
@@ -1,18 +1,35 @@
-module Palettes exposing
-    ( findBySlug
-    , searchByTag
-    )
+module Palettes exposing (findBySlug, searchByTag)
+
+{-| Useful functions for finding palettes from the palette list
+
+@docs findBySlug, searchByTag
+
+-}
 
 import Dict
 import Palette
 import PaletteList exposing (paletteList)
 
 
+{-| Find a palette by "key"
+
+You can easily spot the slug from a Lospec palette url, eg:
+<https://lospec.com/palette-list/oak21> -> "oak21"
+
+-}
 findBySlug : String -> Maybe Palette.Palette
 findBySlug slug =
     Dict.get slug paletteList
 
 
+{-| Return a list of any palettes tagged with the specified string
+
+Keep in mind that since tags are user input, there's no guarantees of consistency.
+You can view the tags for a palette on its Lospec palette page
+
+There's a lot of palettes, so use this carefully - it's probably a tad slow.
+
+-}
 searchByTag : String -> List Palette.Palette
 searchByTag tag =
     Dict.filter (\_ v -> List.member tag <| Palette.getTags v) paletteList
